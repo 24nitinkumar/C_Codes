0f7e7b7c0466d40849b66e96f9045a9df855cca4
Nitin Kumar <73nitin74@gmail.com>
2025-07-06 08:38:48 +0530

    added code for calculator

--- calculator.c
+++ calculator.c
@@ -1,1 +1,18 @@
 #include<stdio.h>
+int main(){
+int a;
+scanf("%d",&a);
+char b;
+scanf("%c",&b);
+int c ;
+scanf("%d",&c);
+if(b=='+')
+printf("%d",a+b);
+if(b=='-')
+printf("%d",a-b);
+if(b=='*')
+printf("%d",a*b);
+if(b=='/')
+printf("%d",a/b);
+return 0;
+}
