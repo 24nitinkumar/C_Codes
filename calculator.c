849b6913fe69d2344eb26b5123709d23684a046e
Nitin Kumar <73nitin74@gmail.com>
2025-07-06 09:00:26 +0530

    error rectified

--- calculator.c
+++ calculator.c
@@ -3,16 +3,17 @@ int main(){
 int a;
 scanf("%d",&a);
 char b;
+getchar();
 scanf("%c",&b);
 int c ;
 scanf("%d",&c);
 if(b=='+')
-printf("%d",a+b);
+printf("%d",a+c);
 if(b=='-')
-printf("%d",a-b);
+printf("%d",a-c);
 if(b=='*')
-printf("%d",a*b);
+printf("%d",a*c);
 if(b=='/')
-printf("%d",a/b);
+printf("%d",a/c);
 return 0;
 }
