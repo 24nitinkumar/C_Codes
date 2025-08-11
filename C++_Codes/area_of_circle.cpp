7f5574fd075a4762a8741c250b6bca39afbc1ee8
Nitin Kumar <73nitin74@gmail.com>
2025-08-11 17:46:15 +0530

    area of circle

---
+++ C++_Codes/area_of_circle.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main (){
+	float area, radius;
+	cout<<"enter the radius of circle" ;
+	cin>>radius;
+	area= 3.14*radius*radius;
+	cout<<"the area of circle is : " <<area;
+}
+	
